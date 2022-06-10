28a7fa448c329e518fc4ae5eb508ab0d21d28c7c
魏朝阳 <435206786@qq.com>
2022-06-10 17:27:51 +0800

    测试reset22

--- src/main/java/com/example/learn/controller/DemoController.java
+++ src/main/java/com/example/learn/controller/DemoController.java
@@ -1,9 +1,16 @@
 package com.example.learn.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @Description:
  * @Author: wcy
  * @CreateDate: 2022/6/10 17:25
  */
 public class DemoController {
+
+    @RequestMapping
+    public void test01() throws Exception {
+
+    }
 }
