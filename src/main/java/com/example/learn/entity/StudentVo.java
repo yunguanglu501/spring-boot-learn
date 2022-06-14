d3e36d3c3fa1044a1f8fc95c34af763baa7edac8
魏朝阳 <435206786@qq.com>
2022-06-14 14:31:05 +0800

    测试回滚

--- src/main/java/com/example/learn/entity/StudentVo.java
+++ src/main/java/com/example/learn/entity/StudentVo.java
@@ -1,8 +1,5 @@
 package com.example.learn.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 /**
@@ -15,22 +12,22 @@ public class StudentVo {
     /**
      * 学号
      */
-    @TableId(value = "stu_id", type = IdType.AUTO)
+
     private String stuId;
 
     /**
      * 学生姓名
      */
-    @TableField(value = "stu_name")
+
     private String stuName;
 
     /**
      * 年龄
      */
-    @TableField(value = "stu_age")
+
     private Integer stuAge;
 
-    @TableField(value = "is_del")
+
     private Integer isDel;
 
 }
